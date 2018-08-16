0bb7dc64ed0be4ea659687ba265e72321e88fa61
Pablo De La Cruz <squall07524@gmail.com>
2018-08-16 15:40:18 -0400

    github info card complete

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
-
 const ApiKey = config.gitHubApiKey;
-let tbody = document.getElementById("tbody");
 
-axios.all([axios.get(`https://api.github.com/users/pablodelacruz1?apiKey=${ApiKey}`), 
-axios.get(`https://api.github.com/users/PabloDeLaCruz1/repos?apiKey=${ApiKey}`)])
+axios.all([axios.get(`https://api.github.com/users/pablodelacruz1?access_token=${ApiKey}`),
+        axios.get(`https://api.github.com/users/PabloDeLaCruz1/repos?access_token=${ApiKey}`)
+    ])
     .then(result => {
+        const tbody = document.getElementById("tbody");
         const res = result[0].data;
         const res2 = result[1].data;
+
         // Set avatar URL
         document.getElementById("avatar_url").setAttribute("src", res.avatar_url);
         //Set profile name
         document.getElementById("github_id").innerHTML = res.login;
         //Set button link to my github
         document.getElementById("github_link").setAttribute("href", res.html_url);
-        
-        //Set repos list 
 
-        console.log("res1", res);
-        console.log("res2", res2);
-
+        //Set total amount of repos and repos list / 
+        document.getElementById("totalRepos").innerHTML = `Total: ${res.public_repos}`;
         res2.forEach(element => {
             let repo = document.createElement("tr");
-            repo.innerHTML = `<td>${element.id}</td>
-            <td>${element.name}</td>`;
+            repo.innerHTML = `<td>
+                                 <a href=${element.html_url}>
+                                      <div>${element.name}</div>
+                                 </a>
+                             </td>`;
 
             tbody.appendChild(repo);
         });
